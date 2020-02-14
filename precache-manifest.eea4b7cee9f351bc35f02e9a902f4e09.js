aab8df7852194b312fc28154b6b3509f899940d3
twinkle <twinkle18@navgurukul.org>
2020-02-14 13:12:07 +0530

    Updates

---
+++ precache-manifest.eea4b7cee9f351bc35f02e9a902f4e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7745668e13bf42d3c91c325001c61bcc",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "bbc8a3be65cdf8a11841",
+    "url": "/deploy/static/css/main.3a872f3e.chunk.css"
+  },
+  {
+    "revision": "d8141881cf889ca20111",
+    "url": "/deploy/static/js/2.c6b0ecb5.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/deploy/static/js/2.c6b0ecb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbc8a3be65cdf8a11841",
+    "url": "/deploy/static/js/main.ffb5e718.chunk.js"
+  },
+  {
+    "revision": "71b5fb1a013b3ccc2a12",
+    "url": "/deploy/static/js/runtime-main.a14133d1.js"
+  }
+]);
